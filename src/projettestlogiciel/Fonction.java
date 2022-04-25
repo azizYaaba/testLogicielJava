24346dfb655edd424bfc0c5ef79bb8faa16dfff4
Tegawende77 <103447691+Tegawende77@users.noreply.github.com>
2022-04-25 02:30:57 +0100

    Ajout des fonctions "estPremier " et  'estParfait"

--- src/projettestlogiciel/Fonction.java
+++ src/projettestlogiciel/Fonction.java
@@ -11,24 +11,52 @@ package projettestlogiciel;
 public class Fonction {
     
     public int pgdc(int m,int n){
-        
-        while(n !=0 ){
-            int r = m%n;
-            m = n;
-            n = r ;
-        }
-        
-        return m;
-    }
+
+	while(n !=0 ){
+	    int r = m%n;
+	    m = n;
+	    n = r ;
+	}
+
+	return m;
+     }
     
-public boolean estPalindrome(String ch){
-	String original = ch;
-	String reverse = "";
-	int taille = original.length();
-	for(int i = taille-1;i>=0;i--)
-		{
-		  reverse = reverse + original.charAt(i); 
+	public boolean estPalindrome(String ch){
+		String original = ch;
+		String reverse = "";
+		int taille = original.length();
+		for(int i = taille-1;i>=0;i--)
+			{
+			  reverse = reverse + original.charAt(i); 
+			}
+		return  original.equals(reverse);
 		}
-	return  original.equals(reverse);
-   	}
+
+	public boolean estPremier(int n){
+		int nbDiviseur = 0;
+		boolean rtr=false;
+		for(int i=1,i<=n;i++){
+			if(n%i==0){
+				nbDiviseur+=nbDiviseur;
+			}
+		}
+		if(nbDiviseur==2){
+			rtr=true;
+		}
+		return rtr;
+	}
+
+	public boolean estParfait(int n){
+		int somDiviseur=0;
+		boolean rtr=false;
+		for(int i=1,i<=n;i++){
+			if(n%i==0){
+				somDiviseur+=i;
+			}
+		}
+		if(somDiviseur==n){
+			boolean rtr=true;
+		}
+		return rtr;
+	}
 }
